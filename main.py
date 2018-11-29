5db0abe2a5a7698dff1ba92ead9f4c18fe8faf22
Rebrina <feurioster@gmail.com>
2018-11-30 01:29:12 +0300

    Версия 2

--- main.py
+++ main.py
@@ -15,30 +15,44 @@ with open('cookbook.txt') as f:
       f.readline()
 print(cook_book)
 
+
 def get_shop_list_by_dishes(dishes, person_count):
     total_ingrigients = {}
-    if dishes in cook_book:
-        i = 0
-        while i <= len(cook_book):
-            for dishes, i in cook_book.items():
-                new_key = cook_book[dishes][i]['ingridient_name']
+    i = 0
+    while i < len(cook_book):
+        dish = dishes[i]
+        print(dish)
+        j = 0
+        if dish in cook_book:
+            while j < len(cook_book[dish]):
+                new_key = cook_book[dish][j]['ingridient_name']
                 print(new_key)
-                quantuty_for_all = int(cook_book[dishes][i]['quantity']) * person_count
-                total_ingrigients[new_key] = {'measure': cook_book[dishes][i]['measure'],
-                                              'quantity': int(cook_book[dishes][i]['quantity']) * person_count}
-                i += 1
-                # total_ingrigients[new_key] = list()
-        # temp = {'measure': cook_book[dishes][0]['measure'], 'quantity': cook_book[dishes][0]['quantity']*person_count}
-        # total_ingrigients[new_key].append(temp)
-        print(total_ingrigients)
+                if new_key in total_ingrigients:
+                    total_ingrigients[new_key]['quantity'] = total_ingrigients[new_key]['quantity'] + int(
+                        cook_book[dish][j]['quantity']) * person_count
+                else:
+                    total_ingrigients[new_key] = {'measure': cook_book[dish][j]['measure'],
+                                                  'quantity': int(cook_book[dish][j]['quantity']) * person_count}
+                j += 1
+        i += 1
+        print(i)
+    print(total_ingrigients)
     return (total_ingrigients)
 
 
-com = input('Введите блюдо')
-com2 = int(input('Введите количество персон'))
+# задание2
+print('#########################')
+dishes = []
+a = ''
+cook_book2 = cook_book
+for keys in cook_book2:
+    a = keys
+    # a = cook_book2.pop(keys)
+    dishes.append(a)
 
-if com and com2:
-    get_shop_list_by_dishes(com, com2)
+print(dishes)
 
-  
+person_count = int(input('Введите кол-во персон'))
+
+get_shop_list_by_dishes(dishes, person_count)
 
