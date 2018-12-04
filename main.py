7bf8348f21c4cddbbd74730927abd577844c0c65
Rebrina <feurioster@gmail.com>
2018-12-04 15:11:57 +0300

    2

--- main.py
+++ main.py
@@ -1,56 +1,50 @@
-i = 0
-cook_book = {}
-cook_book2 = {}
-with open('cookbook.txt') as f:  
-  for line in f:   
-      recipe_id = line.strip()
-      i = int(f.readline())      
-      cook_book[recipe_id] = list()       
-      while i > 0:
-          ingridients = f.readline().strip()                  
-          ingridients_list = ingridients.split(' | ')          
-          cook_book2 = {'ingridient_name': ingridients_list[0], 'quantity':ingridients_list[1], 'measure':ingridients_list[2]}              
-          cook_book[recipe_id].append(cook_book2)    
-          i -= 1   
-      f.readline()
-print(cook_book)
+
+def file_open():
+    i = 0
+    cook_book = {}
+    with open('cookbook.txt', encoding="utf-8") as f:
+      for line in f:
+          recipe_id = line.strip()
+          i = int(f.readline())
+          cook_book[recipe_id] = list()
+          while i > 0:
+              ingridients = f.readline().strip()
+              ingridients_list = ingridients.split(' | ')
+              cook_book[recipe_id].append({'ingridient_name':ingridients_list[0], 'quantity':ingridients_list[1], 'measure':ingridients_list[2]})
+              i -= 1
+          f.readline()
+    return(cook_book)
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     total_ingrigients = {}
     i = 0
-    while i < len(cook_book):
+    cook_book2 = file_open()
+    while i < len(dishes):
         dish = dishes[i]
         print(dish)
         j = 0
-        if dish in cook_book:
-            while j < len(cook_book[dish]):
-                new_key = cook_book[dish][j]['ingridient_name']
+        if dish in cook_book2:
+            while j < len(cook_book2[dish]):
+                new_key = cook_book2[dish][j]['ingridient_name']
                 print(new_key)
                 if new_key in total_ingrigients:
-                    total_ingrigients[new_key]['quantity'] = total_ingrigients[new_key]['quantity'] + int(
-                        cook_book[dish][j]['quantity']) * person_count
+                    total_ingrigients[new_key]['quantity'] = total_ingrigients[new_key]['quantity'] + int(cook_book2[dish][j]['quantity']) * person_count
                 else:
-                    total_ingrigients[new_key] = {'measure': cook_book[dish][j]['measure'],
-                                                  'quantity': int(cook_book[dish][j]['quantity']) * person_count}
+                    total_ingrigients[new_key] = {'measure': cook_book2[dish][j]['measure'], 'quantity': int(cook_book2[dish][j]['quantity']) * person_count}
                 j += 1
         i += 1
         print(i)
     print(total_ingrigients)
     return (total_ingrigients)
 
+#Задание 1
+print(file_open())
 
 # задание2
-print('#########################')
-dishes = []
-a = ''
-cook_book2 = cook_book
-for keys in cook_book2:
-    a = keys
-    # a = cook_book2.pop(keys)
-    dishes.append(a)
+d_list = input('Введите список блюд через запятую', )
 
-print(dishes)
+dishes = d_list.split(', ')
 
 person_count = int(input('Введите кол-во персон'))
 
